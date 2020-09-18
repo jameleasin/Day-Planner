90d66a1f70814be38cf7d7b2b1b5849ba7a937ab
Jamie Sinclair <jrsinclair_28@hotmail.com>
2020-09-18 23:08:58 +0800

    place items into local storage retrieve items from local storage and append to screen

--- planner.js
+++ planner.js
@@ -32,4 +32,28 @@ $(document).ready(function() {
             $(this).addClass("bg-red")
         }
     })
+
+    $("button").on("click", function() {
+        //setting items to local storage
+        localStorage.setItem("9AM", ($text9AM.val()))
+        localStorage.setItem("10AM", ($text10AM.val()))
+        localStorage.setItem("11AM", ($text11AM.val()))
+        localStorage.setItem("12AM", ($text12AM.val()))
+        localStorage.setItem("1PM", ($text1PM.val()))
+        localStorage.setItem("2PM", ($text2PM.val()))
+        localStorage.setItem("3PM", ($text3PM.val()))
+        localStorage.setItem("4PM", ($text4PM.val()))
+        localStorage.setItem("5PM", ($text5PM.val()))
+    })
+
+    //retrieving content and appending it to the screen
+    $("#text9AM").append(localStorage.getItem("9AM"));
+    $("#text10AM").append(localStorage.getItem("10AM"));
+    $("#text11AM").append(localStorage.getItem("11AM"));
+    $("#text12AM").append(localStorage.getItem("12AM"));
+    $("#text1PM").append(localStorage.getItem("1PM"));
+    $("#text2PM").append(localStorage.getItem("2PM"));
+    $("#text3PM").append(localStorage.getItem("3PM"));
+    $("#text4PM").append(localStorage.getItem("4PM"));
+    $("#text5PM").append(localStorage.getItem("4PM"));
 })
