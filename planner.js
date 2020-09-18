ad98a6a6701b5a6928a655bb99acb126ebcf5182
Jamie Sinclair <jrsinclair_28@hotmail.com>
2020-09-18 23:02:04 +0800

    javascript for current date

--- planner.js
+++ planner.js
@@ -4,4 +4,32 @@ $(document).ready(function() {
     var today = moment().format('MMMM Do YYYY');
     console.log(today)
     $('todaysDate').append(today);
+
+    var now = parseInt(moment().format('HH'));
+    console.log(now)
+
+    var $text9AM = $("#text9AM");
+    var $text10AM = $("#text12AM");
+    var $tex11AM = $("#text11AM");
+    var $text12AM = $("#text12AM");
+    var $text1PM = $("#text1PM");
+    var $text2PM = $("#text2PM");
+    var $text3PM = $("#text3PM");
+    var $text4PM = $("#text4PM");
+    var $text5PM = $("#text5PM");
+
+    $("textarea").each(function() {
+        var name = parseInt($(this).attr("name"));
+        if (name < now) {
+            $(this).addClass("bg-gray");
+        }
+
+        if (name > now) {
+            $(this).addClass("bg-green")
+        }
+
+        if (name === now) {
+            $(this).addClass("bg-red")
+        }
+    })
 })
