cff1fbab5069f9f3f8ae56c91e43c3a17f337779
Jamie Sinclair <jrsinclair_28@hotmail.com>
2020-09-22 22:16:23 +0800

    set planner to retrieve time and schedule msg to local storage

--- planner.js
+++ planner.js
@@ -6,6 +6,38 @@ let todaysDateEl = $("#currentDay");
 todaysDateEl.text(moment().format("dddd, MMMM Do"));
 
 //generates time blocks
-let timesArr = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+var hour9 = $('#9');
+var hour10 = $('#10');
+var hour11 = $('#11');
+var hour12 = $('#12');
+var hour1 = $('#01');
+var hour2 = $('#02');
+var hour3 = $('#03');
+var hour4 = $('#04');
+var hour5 = $('#05');
 
-//for loop for the length of the array
+//sets each variable (time block) to the id attribute
+function setPlanner() {
+    todaysDateEl.text(moment().format("dddd, MMMM Do"));
+    $(".timeblock").each(function() {
+        var id = $(this).attr("id");
+
+        if (schedule !== null) {
+            $(this).children(".schedule").val(schedule);
+        }
+    });
+}
+
+setPlanner();
+
+//varialbe equals the id attribute given to the save button
+var saveBtn = $(".saveBtn");
+
+//on button click the time and text in the text area are saved to the local storage
+saveBtn.on("click", function() {
+    event.stopPropogation();
+    var time = $(this).parent().attr("id");
+    var schedule = $(this).siblings(".schedule").val();
+
+    localStorage.setItem(time, schedule);
+});
